c73086a0b2f457de0fe4d5fb958bbc6d527e8e6e
zhaoyaoyuan <zhaoyaoyuan123@163.com>
2026-06-08 11:13:27 +0800

    build: 更新插件名称为 Douyin Capture Pro

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -2,5 +2,7 @@ import { readFileSync, writeFileSync } from "fs";
 
 const manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
 manifest.id = "douyin-capture-pro";
+manifest.name = "Douyin Capture Pro";
 writeFileSync("manifest.json", JSON.stringify(manifest, null, 2) + "\n");
 console.log("[postbuild] manifest.json id set to:", manifest.id);
+console.log("[postbuild] manifest.json name set to:", manifest.name);
