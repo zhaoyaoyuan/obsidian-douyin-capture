193f9328361e67f2e4d640cca39de44438589427
zhaoyaoyuan <zhaoyaoyuan123@163.com>
2026-06-08 11:46:37 +0800

    chore: 升级版本至 1.0.6

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -1,8 +1,13 @@
 import { readFileSync, writeFileSync } from "fs";
 
+const pkg = JSON.parse(readFileSync("package.json", "utf8"));
 const manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
+
 manifest.id = "douyin-capture-pro";
 manifest.name = "Douyin Capture Pro";
+manifest.version = pkg.version;
+
 writeFileSync("manifest.json", JSON.stringify(manifest, null, 2) + "\n");
 console.log("[postbuild] manifest.json id set to:", manifest.id);
 console.log("[postbuild] manifest.json name set to:", manifest.name);
+console.log("[postbuild] manifest.json version set to:", manifest.version);
